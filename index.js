1261bc7b44019b19362a2426b7ec7e7bb161077a
Gagan Chaudhary <singhgaganbtp@gmail.com>
2022-12-28 23:00:58 +0530

    changed css

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=London%252Cuk", {
-  "method": "GET",
-  "headers": {
-    "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-    "x-rapidapi-key": "[your rapidapi key]"
-  }
-})
-.then(response => {
-  console.log(response);
-})
-.catch(err => {
-  console.log(err);
-});
+let buttonBeforeEl=document.getElementById("button-before");
+let buttonAfterEl=document.getElementById("button-after");
+let textEl=document.getElementById("textBox");
